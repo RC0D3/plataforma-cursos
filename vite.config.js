0e8bc54e6564dd4a235b7cb7d035e7dc1235359d
RC0D3 <rc0d3m4ax@gmail.com>
2023-12-05 16:32:38 -0300

    NO VERCEL HERE MORE

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    base: "https://plataforma-cursos-teste.vercel.app/",
-
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
