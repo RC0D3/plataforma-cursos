08eb8c5809c0b54868925b00a19bbcb23cbebc89
RC0D3 <rc0d3m4ax@gmail.com>
2023-12-05 16:23:35 -0300

    resolvendo https

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    base: "https://plataforma-cursos-teste.vercel.app/",
+
     plugins: [
         laravel({
             input: ["resources/css/app.css", "resources/js/app.js"],
